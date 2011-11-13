81206e5383a96d90563875dcbc3ccb510cdbdb80
Aaron Small <a.small@unb.ca>
2011-11-13 18:17:55 -0500

    Change shell to use. Before it was /bin/sh unless the application overrides it. Now, first use application specified. If nothing specified, check the passwd file. If there's nothing there either, fall back to /bin/sh

--- lib/fork.c
+++ lib/fork.c
@@ -3,6 +3,7 @@
 #else
 #include <pty.h>
 #endif
+#include <pwd.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
@@ -11,49 +12,60 @@
 
 bool term_fork(term_t_i *term)
 {
+	bool inspace;
+	int count = 1;
+	char *ptr;
+	char **args;
+	char *shell;
 	int pid;
+	struct passwd *passwd;
 	pid = forkpty(&term->fd, NULL, NULL, NULL);
 	
 	if(pid < 0 ) {
 		return false;
 	} else if( pid == 0 ) {
+		// Default shell if no other is specified
+		shell = "/bin/sh -l";
+		// See if the application is overriding the shell
 		if( term->shell != NULL ) {
-			bool inspace;
-			int count = 1;
-			char *ptr;
-			char **args;
-
-			// Count the tokens
-			for( inspace = true, ptr = term->shell; *ptr != '\0'; ptr ++ ) {
-				if( *ptr != ' ' && inspace ) {
-					count ++;
-				}
-				inspace = (*ptr == ' ');
-			}
-
-			// Allocate space for tokens
-			args = malloc( sizeof( char * ) * count );
-
-			// Package the tokens
-			count = 0;
-			for( inspace = true, ptr = term->shell; *ptr != '\0'; ptr ++ ) {
-				if( *ptr != ' ' && inspace ) {
-					args[ count ] = ptr;
-					count ++;
-				} else if ( *ptr == ' ' && !inspace ) {
-					*ptr = '\0';
-					inspace = true;
-					continue;
-				}
-				inspace = (*ptr == ' ');
-			}
-
-			// And a terminator
-			args[ count ] = NULL;
-			execvp( args[ 0 ], args );
+			shell = term->shell;
 		} else {
-			execl("/bin/sh", "/bin/sh", "-l", NULL);
+			// See if the environment has a shell set
+			passwd = getpwuid( getuid( ) );
+			if( passwd != NULL && passwd->pw_shell != NULL ) {
+				shell = passwd->pw_shell;
+			}
 		}
+
+		// Break up the command line where there are spaces
+		// Count the tokens
+		for( inspace = true, ptr = shell; *ptr != '\0'; ptr ++ ) {
+			if( *ptr != ' ' && inspace ) {
+				count ++;
+			}
+			inspace = (*ptr == ' ');
+		}
+
+		// Allocate space for tokens
+		args = malloc( sizeof( char * ) * count );
+
+		// Package the tokens
+		count = 0;
+		for( inspace = true, ptr = shell; *ptr != '\0'; ptr ++ ) {
+			if( *ptr != ' ' && inspace ) {
+				args[ count ] = ptr;
+				count ++;
+			} else if ( *ptr == ' ' && !inspace ) {
+				*ptr = '\0';
+				inspace = true;
+				continue;
+			}
+			inspace = (*ptr == ' ');
+		}
+
+		// And a terminator
+		args[ count ] = NULL;
+		execvp( args[ 0 ], args );
 	} else {
 		term->child = pid;
 	}
