3a4650de016fa93486c278660729de45547d96cc
Aaron Small <aaron.small.99a@gmail.com>
2011-11-14 17:58:37 -0500

    Tabs to spaces

--- cterm/cterm.c
+++ cterm/cterm.c
@@ -4,74 +4,74 @@
 #include <libterm.h>
 #include <sys/select.h>
 
-#define WIDTH	80
-#define HEIGHT	25
+#define WIDTH    80
+#define HEIGHT    25
 
 WINDOW *wnd;
 
 void update(term_t handle, int x, int y, int width, int height)
 {
-	int i, j;
-	const uint32_t **grid = term_get_grid(handle);
+    int i, j;
+    const uint32_t **grid = term_get_grid(handle);
 
 
-	for( i = 0; i < HEIGHT; i ++ ) {
-		for( j = 0; j < WIDTH; j ++ ) {
-			wmove( wnd, i, j );
-			wdelch( wnd );
-			winsch( wnd, grid[i][j] );
-		}
-	}
-	wrefresh(wnd);
+    for( i = 0; i < HEIGHT; i ++ ) {
+        for( j = 0; j < WIDTH; j ++ ) {
+            wmove( wnd, i, j );
+            wdelch( wnd );
+            winsch( wnd, grid[i][j] );
+        }
+    }
+    wrefresh(wnd);
 }
 
 int main(int argc, char *argv[])
 {
-	int length;
-	fd_set rfds;
-	int file_handle;
-	int stdin_handle;
-	term_t handle;
-	WINDOW *screen;
+    int length;
+    fd_set rfds;
+    int file_handle;
+    int stdin_handle;
+    term_t handle;
+    WINDOW *screen;
 
-	screen = initscr();
+    screen = initscr();
 
-	wnd = newwin(HEIGHT, WIDTH, 0, 0);
-	wclear( wnd );
+    wnd = newwin(HEIGHT, WIDTH, 0, 0);
+    wclear( wnd );
 
-	term_create(&handle);
-	term_set_shell(handle, "/bin/bash");
-	term_begin(handle, WIDTH, HEIGHT, 0);
+    term_create(&handle);
+    term_set_shell(handle, "/bin/bash");
+    term_begin(handle, WIDTH, HEIGHT, 0);
 
-	term_register_update(handle, update);
-	stdin_handle = fileno(stdin);
-	file_handle = term_get_file_descriptor(handle);
+    term_register_update(handle, update);
+    stdin_handle = fileno(stdin);
+    file_handle = term_get_file_descriptor(handle);
 
-	while( 1 ) {
-		FD_ZERO(&rfds);
-		FD_SET(file_handle, &rfds);
-		FD_SET(stdin_handle, &rfds);
+    while( 1 ) {
+        FD_ZERO(&rfds);
+        FD_SET(file_handle, &rfds);
+        FD_SET(stdin_handle, &rfds);
 
-		select( file_handle+1, &rfds, NULL, NULL, NULL );
+        select( file_handle+1, &rfds, NULL, NULL, NULL );
 
-		if(FD_ISSET(file_handle, &rfds)) {
-			if( !term_process_child(handle) ) {
-				break;
-			}
-		} else if(FD_ISSET(stdin_handle, &rfds)) {
-			char buf[100];
-			length = read(stdin_handle, buf, 100);
-			term_send_data(handle, buf, length);
-		}
+        if(FD_ISSET(file_handle, &rfds)) {
+            if( !term_process_child(handle) ) {
+                break;
+            }
+        } else if(FD_ISSET(stdin_handle, &rfds)) {
+            char buf[100];
+            length = read(stdin_handle, buf, 100);
+            term_send_data(handle, buf, length);
+        }
 
-	}
+    }
 
-	term_free( handle );
+    term_free( handle );
 
-	delwin( wnd );
-	delwin( screen );
+    delwin( wnd );
+    delwin( screen );
 
-	endwin( );
+    endwin( );
 
-	return 0;
+    return 0;
 }
--- lib/render.c
+++ lib/render.c
@@ -3,56 +3,56 @@
 
 void term_process_output_data(term_t_i *term, char *buf, int length)
 {
-	bool change = false;
-	bool cursor_change = false;
-	int i;
+    bool change = false;
+    bool cursor_change = false;
+    int i;
 
-	for( i = 0; i < length; i ++ ) {
+    for( i = 0; i < length; i ++ ) {
 #if 0
-		if( isgraph( buf[ i ] ) ) {
-			printf( "Output character %d (%c)\n", buf[ i ], buf[ i ] );
-		} else {
-			printf( "Output character %d\n", buf[ i ] );
-		}
+        if( isgraph( buf[ i ] ) ) {
+            printf( "Output character %d (%c)\n", buf[ i ], buf[ i ] );
+        } else {
+            printf( "Output character %d\n", buf[ i ] );
+        }
 #endif
 
-		if( term->escape_mode ) {
-			change = true;
-			i += term_send_escape( term, buf + i, length  - i );
-			if( i == length ) break;
-		}
-		switch(buf[i]) {
-			case '\b':
-				term->ccol--;
-				cursor_change = true;
-				break;
-			case '\r':
-				break;
-			case '\n':
-				term->ccol=0;
-				term->crow++;
-				cursor_change = true;
-				if( term->crow >= term->height ) {
-					term_shiftrows(term);
-					change = true;
-				}
-				break;
-			case 27:
-				term->escape_mode = true;
-				term_send_escape( term, buf + i, 1 );
-				break;
-			default:
-				if( term->crow < term->height && term->ccol < term->width ) {
-					term->grid[term->crow][term->ccol] = buf[i];
-					term->attribs[term->crow][term->ccol] = term->cattr;
-					term->colours[term->crow][term->ccol] = term->ccolour;
-					change = true;
-				}
-				term->ccol++;
-				cursor_change = true;
-		}
-	}
+        if( term->escape_mode ) {
+            change = true;
+            i += term_send_escape( term, buf + i, length  - i );
+            if( i == length ) break;
+        }
+        switch(buf[i]) {
+            case '\b':
+                term->ccol--;
+                cursor_change = true;
+                break;
+            case '\r':
+                break;
+            case '\n':
+                term->ccol=0;
+                term->crow++;
+                cursor_change = true;
+                if( term->crow >= term->height ) {
+                    term_shiftrows(term);
+                    change = true;
+                }
+                break;
+            case 27:
+                term->escape_mode = true;
+                term_send_escape( term, buf + i, 1 );
+                break;
+            default:
+                if( term->crow < term->height && term->ccol < term->width ) {
+                    term->grid[term->crow][term->ccol] = buf[i];
+                    term->attribs[term->crow][term->ccol] = term->cattr;
+                    term->colours[term->crow][term->ccol] = term->ccolour;
+                    change = true;
+                }
+                term->ccol++;
+                cursor_change = true;
+        }
+    }
 
-	if( change && term->update != NULL ) term->update(TO_H(term), 0, 0, term->width, term->height);
-	if( cursor_change && term->cursor_update != NULL ) term->cursor_update(TO_H(term), term->ccol, term->crow);
+    if( change && term->update != NULL ) term->update(TO_H(term), 0, 0, term->width, term->height);
+    if( cursor_change && term->cursor_update != NULL ) term->cursor_update(TO_H(term), term->ccol, term->crow);
 }
