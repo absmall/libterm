fdd132a57728395985f77e779d6d91d0686265ab
Aaron Small <aaron.small.99a@gmail.com>
2011-11-22 09:15:44 -0500

    Switch tabs back to spaces. The program it caused before was a bug in Qt which has been fixed.

--- lib/fork.c
+++ lib/fork.c
@@ -17,109 +17,109 @@
 
 bool term_fork(term_t_i *term)
 {
-	bool inspace;
-	int count = 2;
-	char *ptr;
-	char **args;
-	char *shell;
-	int status;
-	int pid;
-	int read_byte;
-	int pipefd[2];
-	struct passwd *passwd;
+    bool inspace;
+    int count = 2;
+    char *ptr;
+    char **args;
+    char *shell;
+    int status;
+    int pid;
+    int read_byte;
+    int pipefd[2];
+    struct passwd *passwd;
 
-	if( pipe( pipefd ) ) {
-		return false;
-	}
+    if( pipe( pipefd ) ) {
+        return false;
+    }
 
-	pid = forkpty(&term->fd, NULL, NULL, NULL);
+    pid = forkpty(&term->fd, NULL, NULL, NULL);
 
-	if(pid < 0 ) {
-		return false;
-	} else if( pid == 0 ) {
-		// Default shell if no other is specified
-		shell = "/bin/sh";
-		// See if the application is overriding the shell
-		if( term->shell != NULL ) {
-			shell = term->shell;
-		} else {
-			// See if the environment has a shell set
-			passwd = getpwuid( getuid( ) );
-			if( passwd != NULL && passwd->pw_shell != NULL ) {
-				shell = passwd->pw_shell;
-			}
-		}
+    if(pid < 0 ) {
+        return false;
+    } else if( pid == 0 ) {
+        // Default shell if no other is specified
+        shell = "/bin/sh";
+        // See if the application is overriding the shell
+        if( term->shell != NULL ) {
+            shell = term->shell;
+        } else {
+            // See if the environment has a shell set
+            passwd = getpwuid( getuid( ) );
+            if( passwd != NULL && passwd->pw_shell != NULL ) {
+                shell = passwd->pw_shell;
+            }
+        }
 
-		switch( term->type ) {
-			case TERM_TYPE_VT100:
-				setenv("TERM", "vt100", 1);
-				break;
-		}
+        switch( term->type ) {
+            case TERM_TYPE_VT100:
+                setenv("TERM", "vt100", 1);
+                break;
+        }
 
-		close( pipefd[ 0 ] );
-		fcntl( pipefd[ 1 ], F_SETFD, FD_CLOEXEC );
+        close( pipefd[ 0 ] );
+        fcntl( pipefd[ 1 ], F_SETFD, FD_CLOEXEC );
 
-		// Break up the command line where there are spaces
-		// Count the tokens
-		for( inspace = true, ptr = shell; *ptr != '\0'; ptr ++ ) {
-			if( *ptr != ' ' && inspace ) {
-				count ++;
-			}
-			inspace = (*ptr == ' ');
-		}
+        // Break up the command line where there are spaces
+        // Count the tokens
+        for( inspace = true, ptr = shell; *ptr != '\0'; ptr ++ ) {
+            if( *ptr != ' ' && inspace ) {
+                count ++;
+            }
+            inspace = (*ptr == ' ');
+        }
 
-		// Allocate space for tokens
-		args = malloc( sizeof( char * ) * count );
-		if( args == NULL ) {
-			exit(1);
-		}
+        // Allocate space for tokens
+        args = malloc( sizeof( char * ) * count );
+        if( args == NULL ) {
+            exit(1);
+        }
 
-		// Package the tokens
-		count = 0;
-		for( inspace = true, ptr = shell; *ptr != '\0'; ptr ++ ) {
-			if( *ptr != ' ' && inspace ) {
-				args[ count ] = ptr;
-				count ++;
-			} else if ( *ptr == ' ' && !inspace ) {
-				*ptr = '\0';
-				inspace = true;
-				continue;
-			}
-			inspace = (*ptr == ' ');
-		}
+        // Package the tokens
+        count = 0;
+        for( inspace = true, ptr = shell; *ptr != '\0'; ptr ++ ) {
+            if( *ptr != ' ' && inspace ) {
+                args[ count ] = ptr;
+                count ++;
+            } else if ( *ptr == ' ' && !inspace ) {
+                *ptr = '\0';
+                inspace = true;
+                continue;
+            }
+            inspace = (*ptr == ' ');
+        }
 
-		// And a terminator
-		args[ count++ ] = "-l";
-		args[ count ] = NULL;
-		execvp( args[ 0 ], args );
-		status = errno;
-		write( pipefd[ 1 ], &status, 1 );
-		exit( status );
-	} else {
-		close( pipefd[ 1 ] );
-		term->child = pid;
-		while( 1 ) {
-			read_byte = read( pipefd[ 0 ], &status, 1 );
-			if( read_byte == 1 ) {
-				waitpid( term->child, &status, 0 );
-				close( pipefd[ 0 ] );
-				errno = WEXITSTATUS( status );
-				return false;
-			} else {
-				if( errno == EINTR ) {
-					continue;
-				} else {
-					break;
-				}
-			}
-		}
-	}
-	return true;
+        // And a terminator
+        args[ count++ ] = "-l";
+        args[ count ] = NULL;
+        execvp( args[ 0 ], args );
+        status = errno;
+        write( pipefd[ 1 ], &status, 1 );
+        exit( status );
+    } else {
+        close( pipefd[ 1 ] );
+        term->child = pid;
+        while( 1 ) {
+            read_byte = read( pipefd[ 0 ], &status, 1 );
+            if( read_byte == 1 ) {
+                waitpid( term->child, &status, 0 );
+                close( pipefd[ 0 ] );
+                errno = WEXITSTATUS( status );
+                return false;
+            } else {
+                if( errno == EINTR ) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    return true;
 }
 
 void term_slay(term_t_i *term)
 {
-	if( term->child != 0 ) {
-		kill(term->child, SIGTERM);
-	}
+    if( term->child != 0 ) {
+        kill(term->child, SIGTERM);
+    }
 }
--- lib/memory.c
+++ lib/memory.c
@@ -5,145 +5,145 @@
 
 bool term_allocate_grid(term_t_i *term)
 {
-	int i, j;
+    int i, j;
 
-	term->grid = malloc(sizeof(uint32_t *)*term->history);
-	if( term->grid == NULL ) {
-		free(term);
-		errno = ENOMEM;
-		return false;
-	}
-	term->attribs = malloc(sizeof(uint32_t *)*term->history);
-	if( term->attribs == NULL ) {
-		free(term->grid);
-		free(term);
-		errno = ENOMEM;
-		return false;
-	}
-	term->colours = malloc(sizeof(uint32_t *)*term->history);
-	if( term->colours == NULL ) {
-		free(term->attribs);
-		free(term->grid);
-		free(term);
-		errno = ENOMEM;
-		return false;
-	}
-	for( i = 0; i < term->history; i ++ ) {
-		term->grid[i] = malloc(sizeof(uint32_t)*term->width);
-		if( term->grid[i] == NULL ) {
-			for(i--; i>=0; i--) {
-				free(term->grid[i]);
-			}
-			free(term->grid[0]);
-			free(term->colours);
-			free(term->attribs);
-			free(term->grid);
-			free(term);
-			errno = ENOMEM;
-			return false;
-		}
-		term->attribs[i] = malloc(sizeof(uint32_t)*term->width);
-		if( term->attribs[i] == NULL ) {
-			free(term->grid[i]);
-			for(i--; i>=0; i--) {
-				free(term->attribs[i]);
-				free(term->grid[i]);
-			}
-			free(term->attribs[0]);
-			free(term->grid[0]);
-			free(term->colours);
-			free(term->attribs);
-			free(term->grid);
-			free(term);
-			errno = ENOMEM;
-			return false;
-		}
-		term->colours[i] = malloc(sizeof(uint32_t)*term->width);
-		if( term->colours[i] == NULL ) {
-			free(term->attribs[i]);
-			free(term->grid[i]);
-			for(i--; i>=0; i--) {
-				free(term->colours[i]);
-				free(term->attribs[i]);
-				free(term->grid[i]);
-			}
-			free(term->grid[0]);
-			free(term->attribs[0]);
-			free(term->colours);
-			free(term->attribs);
-			free(term->grid);
-			free(term);
-			errno = ENOMEM;
-			return false;
-		}
-		for( j = 0; j < term->width; j ++ ) {
-			term->grid[ i ][ j ] = ' ';
-		}
-		memset(term->attribs[i], 0, sizeof(uint32_t)*term->width);
-		memset(term->colours[i], 0, sizeof(uint32_t)*term->width);
-	}
+    term->grid = malloc(sizeof(uint32_t *)*term->history);
+    if( term->grid == NULL ) {
+        free(term);
+        errno = ENOMEM;
+        return false;
+    }
+    term->attribs = malloc(sizeof(uint32_t *)*term->history);
+    if( term->attribs == NULL ) {
+        free(term->grid);
+        free(term);
+        errno = ENOMEM;
+        return false;
+    }
+    term->colours = malloc(sizeof(uint32_t *)*term->history);
+    if( term->colours == NULL ) {
+        free(term->attribs);
+        free(term->grid);
+        free(term);
+        errno = ENOMEM;
+        return false;
+    }
+    for( i = 0; i < term->history; i ++ ) {
+        term->grid[i] = malloc(sizeof(uint32_t)*term->width);
+        if( term->grid[i] == NULL ) {
+            for(i--; i>=0; i--) {
+                free(term->grid[i]);
+            }
+            free(term->grid[0]);
+            free(term->colours);
+            free(term->attribs);
+            free(term->grid);
+            free(term);
+            errno = ENOMEM;
+            return false;
+        }
+        term->attribs[i] = malloc(sizeof(uint32_t)*term->width);
+        if( term->attribs[i] == NULL ) {
+            free(term->grid[i]);
+            for(i--; i>=0; i--) {
+                free(term->attribs[i]);
+                free(term->grid[i]);
+            }
+            free(term->attribs[0]);
+            free(term->grid[0]);
+            free(term->colours);
+            free(term->attribs);
+            free(term->grid);
+            free(term);
+            errno = ENOMEM;
+            return false;
+        }
+        term->colours[i] = malloc(sizeof(uint32_t)*term->width);
+        if( term->colours[i] == NULL ) {
+            free(term->attribs[i]);
+            free(term->grid[i]);
+            for(i--; i>=0; i--) {
+                free(term->colours[i]);
+                free(term->attribs[i]);
+                free(term->grid[i]);
+            }
+            free(term->grid[0]);
+            free(term->attribs[0]);
+            free(term->colours);
+            free(term->attribs);
+            free(term->grid);
+            free(term);
+            errno = ENOMEM;
+            return false;
+        }
+        for( j = 0; j < term->width; j ++ ) {
+            term->grid[ i ][ j ] = ' ';
+        }
+        memset(term->attribs[i], 0, sizeof(uint32_t)*term->width);
+        memset(term->colours[i], 0, sizeof(uint32_t)*term->width);
+    }
 
-	term->allocated = true;
+    term->allocated = true;
 
-	return true;
+    return true;
 }
 
 void term_shiftrows(term_t_i *term)
 {
-	uint32_t *firstrow;
-	int i;
+    uint32_t *firstrow;
+    int i;
 
-	// Just cycle the pointers, and move the first row to the end, but clear it
-	// grid
-	firstrow = term->grid[0];
-	for( i = 1; i < term->history; i ++ ) {
-		term->grid[ i - 1 ] = term->grid[ i ];
-	}
-	term->grid[ i - 1 ] = firstrow;
+    // Just cycle the pointers, and move the first row to the end, but clear it
+    // grid
+    firstrow = term->grid[0];
+    for( i = 1; i < term->history; i ++ ) {
+        term->grid[ i - 1 ] = term->grid[ i ];
+    }
+    term->grid[ i - 1 ] = firstrow;
 
-	for( i = 0; i < term->width; i ++ ) {
-		firstrow[ i ] = ' ';
-	}
+    for( i = 0; i < term->width; i ++ ) {
+        firstrow[ i ] = ' ';
+    }
 
-	// attribs
-	firstrow = term->attribs[0];
-	for( i = 1; i < term->history; i ++ ) {
-		term->attribs[ i - 1 ] = term->attribs[ i ];
-	}
-	term->attribs[ i - 1 ] = firstrow;
+    // attribs
+    firstrow = term->attribs[0];
+    for( i = 1; i < term->history; i ++ ) {
+        term->attribs[ i - 1 ] = term->attribs[ i ];
+    }
+    term->attribs[ i - 1 ] = firstrow;
 
-	for( i = 0; i < term->width; i ++ ) {
-		firstrow[ i ] = 0;
-	}
+    for( i = 0; i < term->width; i ++ ) {
+        firstrow[ i ] = 0;
+    }
 
-	// colours
-	firstrow = term->colours[0];
-	for( i = 1; i < term->history; i ++ ) {
-		term->colours[ i - 1 ] = term->colours[ i ];
-	}
-	term->colours[ i - 1 ] = firstrow;
+    // colours
+    firstrow = term->colours[0];
+    for( i = 1; i < term->history; i ++ ) {
+        term->colours[ i - 1 ] = term->colours[ i ];
+    }
+    term->colours[ i - 1 ] = firstrow;
 
-	for( i = 0; i < term->width; i ++ ) {
-		firstrow[ i ] = 0;
-	}
+    for( i = 0; i < term->width; i ++ ) {
+        firstrow[ i ] = 0;
+    }
 
-	term->crow --;
+    term->crow --;
 }
 
 void term_release_grid(term_t_i *term)
 {
-	int i;
+    int i;
 
-	if( !term->allocated ) return;
-	for( i = 0; i < term->history; i ++ ) {
-		free(term->grid[i]);
-		free(term->attribs[i]);
-		free(term->colours[i]);
-	}
-	free(term->colours);
-	free(term->attribs);
-	free(term->grid);
+    if( !term->allocated ) return;
+    for( i = 0; i < term->history; i ++ ) {
+        free(term->grid[i]);
+        free(term->attribs[i]);
+        free(term->colours[i]);
+    }
+    free(term->colours);
+    free(term->attribs);
+    free(term->grid);
 
-	term->allocated = false;
+    term->allocated = false;
 }
 
