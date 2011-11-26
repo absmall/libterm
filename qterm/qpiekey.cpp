0a35bb91b228dcb21fe37eadd52656625abb7930
Aaron Small <a.small@unb.ca>
2011-11-26 17:43:38 -0500

    Highlight a section when the mouse enters that section

--- qterm/qpiekey.cpp
+++ qterm/qpiekey.cpp
@@ -26,9 +26,13 @@ void QPieKey::paintEvent(QPaintEvent *event)
 
     painter.setBrush(QColor(128, 128, 128));
     painter.drawRect(event->rect());
-    painter.setBrush(QColor(0, 0, 0));
 
     if( sections ) {
+        painter.setBrush(QColor(150, 150, 150));
+        if( highlighted_section != -1 ) {
+            painter.drawPie(0, 0, size*2, size*2, (90-(angle*(highlighted_section+0.5))*180/M_PI)*16, angle*180/M_PI*16);
+        }
+        painter.setBrush(QColor(0, 0, 0));
         for( i = 0; i < sections; i ++ ) {
             for( j = 0; j < sections; j ++ ) {
                 // The angle of the section extends from angle*(i-0.5) to
@@ -42,8 +46,31 @@ void QPieKey::paintEvent(QPaintEvent *event)
             }
         }
         for( i = 0; i < sections; i ++ ) {
-            painter.drawLine(size, size, size+size*sin(angle*(i+0.5)), size-size*cos(angle*(i+0.5)));
+            if( highlighted_section == -1 || (i != highlighted_section && i != (highlighted_section + 1) % sections)) {
+                painter.drawLine(size, size, size+size*sin(angle*(i-0.5)), size-size*cos(angle*(i-0.5)));
+            }
         }
+    }
+}
+
+void QPieKey::mouseMoveEvent(QMouseEvent *event)
+{
+    int section;
+    double distance;
+    double mouseangle;
+    // Find whether we are in the wheel
+    distance = sqrt((event->x()-size)*(event->x()-size)+(event->y()-size)*(event->y()-size));
+
+    if( distance * 2 < size ) {
+        section = -1;
+    } else {
+        mouseangle = atan2(((double)size - event->x()), ((double)event->y() - size)) + M_PI;
+        section = ((int)((mouseangle + 0.5) / angle)) % sections;
+    }
+
+    if( section != highlighted_section ) {
+        highlighted_section = section;
+        update(0,0, size*2, size*2);
     }
 }
 
