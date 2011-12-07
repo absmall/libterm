acea80b984aa2d2672b2d609c97723bca130fabd
Aaron Small <aaron.small.99a@gmail.com>
2011-12-07 17:25:18 -0500

    Fix a couple uses of uninitialized variables. This fixes the crash I was seeing, and the descent problem

--- qterm/qterm.cpp
+++ qterm/qterm.cpp
@@ -71,9 +71,12 @@ void QTerm::init()
     if(char_width==0) {
         fontWorkAround = true;
         char_width = metrics.width(QChar('X'));
+    } else {
+        fontWorkAround = false;
     }
 
     char_height = metrics.lineSpacing();
+    char_descent = metrics.descent();
 
 #ifdef __QNX__
     BlackBerry::Keyboard::instance().show();
