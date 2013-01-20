6d3b9752350bc35423f14ab5b10a45df27a4ff36
Aaron Small <aaron.small@shadnet.shad.ca>
2013-01-20 14:19:08 -0500

    Make use of the socket - display now shows the prompt initially, but updates will be wrong, and no data sent yet

--- casterm/terminalscreen.cpp
+++ casterm/terminalscreen.cpp
@@ -7,26 +7,40 @@ TerminalScreen::TerminalScreen( term_t handle, int argc, char *argv[], AbstractP
 {
     int ret = 0;
     QObject *pObj;
-    QVariantMap map;
 
+    term_get_grid_size( handle, &width, &height );
+    slog2f(buffer_handle, 0, SLOG2_INFO, "TerminalScreen start %dx%d", width, height);
     mRecordingDeviceListModel = new QVariantListDataModel;
-    //term_set_user_data( terminal, this );
-    //term_register_update( terminal, term_update );
+    term_set_user_data( terminal, this );
+    term_register_update( terminal, term_update );
     pObj = pane->findChild<QObject *>("terminalLines");
     mRecordingDeviceList = (ListView *) pObj;
-    for( int i = 0; i < 3; i ++ ) {
-        map["line"] = QString("Hello");
-        *mRecordingDeviceListModel << map;
-    }
     mRecordingDeviceList->setDataModel( mRecordingDeviceListModel );
+
+    notifier = new QSocketNotifier( term_get_file_descriptor(terminal), QSocketNotifier::Read );
+    QObject::connect(notifier, SIGNAL(activated(int)), this, SLOT(terminal_data()));
 }
 
 TerminalScreen::~TerminalScreen()
 {
+    delete notifier;
 }
 
 void TerminalScreen::term_update( term_t term, int x, int y, int width, int height )
 {
+    QVariantMap map;
+
     TerminalScreen *ts = (TerminalScreen *)term_get_user_data( term );
-    slog2fa(buffer_handle, 0, SLOG2_INFO, "terminal update");
+    slog2c(buffer_handle, 0, SLOG2_INFO, "terminal update");
+    for( int i = 0; i < height; i ++ ) {
+        map["line"] = QString(term_get_line(ts->terminal, i));
+        *ts->mRecordingDeviceListModel << map;
+    }
+}
+
+void TerminalScreen::terminal_data()
+{
+    if( !term_process_child( terminal ) ) {
+        exit(0);
+    }
 }
--- casterm/terminalscreen.hpp
+++ casterm/terminalscreen.hpp
@@ -6,16 +6,23 @@
 #include <bb/cascades/QListDataModel>
 #include <libterm.h>
 
-class TerminalScreen
+class TerminalScreen : public QObject
 {
+    Q_OBJECT
 public:
 	TerminalScreen( term_t handle, int argc, char *argv[], bb::cascades::AbstractPane *);
 	~TerminalScreen();
 private:
     static void term_update( term_t term, int x, int y, int width, int height );
+
+    int width;
+    int height;
     term_t terminal;
+    QSocketNotifier *notifier;
 	bb::cascades::ListView *mRecordingDeviceList;
 	bb::cascades::QVariantListDataModel *mRecordingDeviceListModel;
+private slots:
+    void terminal_data();
 };
 
 #endif
